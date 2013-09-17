dd23475937fb161caeda463415ecaf03f4e03c5e
dlee0113 <dlee0113@gmail.com>
2013-09-17 14:27:29 -0400

    added comments and print statements

--- spring-tutorial-11/src/main/java/com/caveofprogramming/spring/test/App.java
+++ spring-tutorial-11/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -10,12 +10,15 @@ public class App {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
 		
+		// Since bean scope is singletone, both person1 and person2 refer to the same object
 		Person person1 = (Person)context.getBean("person");
 		Person person2 = (Person)context.getBean("person");
 		
+		// Both person1 and person2 are updated
 		person1.setTaxId(666);
 
-		System.out.println(person2);
+		System.out.println(person1);
+		System.out.println(person1);
 
 		((ClassPathXmlApplicationContext)context).close();
 	}
