1d0d330998566b353d43d6bbfd9f3b22e6cd0b56
dlee0113 <dlee0113@gmail.com>
2013-09-17 14:19:34 -0400

    added comments and addtional print statements

--- spring-tutorial-10/src/main/java/com/caveofprogramming/spring/test/App.java
+++ spring-tutorial-10/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -2,20 +2,24 @@ package com.caveofprogramming.spring.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
-
 	public static void main(String[] args) {
-		
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
 		
+		// If bean scope is singleton, person1 and person2 refer to the same object.
+		// If bean scope is prototype, person1 and person2 refer to a new object, respectively.
 		Person person1 = (Person)context.getBean("person");
 		Person person2 = (Person)context.getBean("person");
 		
-		person1.setTaxId(666);
+		System.out.println("person1: " + person1);
+		System.out.println("person2: " + person2);
+		
+		// Since person1 and person2 are 2 different objects, only person1 is changed.
+		person1.setId(000);
 
-		System.out.println(person2);
+		System.out.println("person1: " + person1);
+		System.out.println("person2: " + person2);
 		
 		((ClassPathXmlApplicationContext)context).close();
 	}
