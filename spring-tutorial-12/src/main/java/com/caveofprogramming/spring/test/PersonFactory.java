570fc63c5d38332183b021aa826872cca10b86f2
dlee0113 <dlee0113@gmail.com>
2013-09-17 14:41:25 -0400

    added a comment

--- spring-tutorial-12/src/main/java/com/caveofprogramming/spring/test/PersonFactory.java
+++ spring-tutorial-12/src/main/java/com/caveofprogramming/spring/test/PersonFactory.java
@@ -1,6 +1,7 @@
 package com.caveofprogramming.spring.test;
 
 public class PersonFactory {
+	// This method gets called whenever person bean gets instantiated.
 	public Person createPerson(int id, String name) {
 		System.out.println("Using factory to create person.");
 		return new Person(id, name);
