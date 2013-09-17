7c52df44a130a8a02dfadb04c654ef67a6274874
dlee0113 <dlee0113@gmail.com>
2013-09-17 14:02:56 -0400

    cleaned up space

--- spring-tutorial-5/src/main/java/com/caveofprogramming/spring/test/App.java
+++ spring-tutorial-5/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -4,9 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
-
 	public static void main(String[] args) {
-		
 		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
 		
 		Person person = (Person)context.getBean("person");
