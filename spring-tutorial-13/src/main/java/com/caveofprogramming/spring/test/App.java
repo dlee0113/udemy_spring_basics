8950f38be5da044acda29e704ce75afcee313842
dlee0113 <dlee0113@gmail.com>
2013-09-18 07:43:54 -0400

    updates

--- spring-tutorial-13/src/main/java/com/caveofprogramming/spring/test/App.java
+++ spring-tutorial-13/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -2,24 +2,23 @@ package com.caveofprogramming.spring.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
-
 	public static void main(String[] args) {
-		
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
 		
-		Person person = (Person)context.getBean("person");
+		Person person1 = (Person)context.getBean("person1");
+		System.out.println(person1);
 		
-		person.setTaxId(666);
-
-		System.out.println(person);
+		Person person2 = (Person)context.getBean("person2");
+		System.out.println(person2);
+		
+		Address address1 = (Address)context.getBean("address1");
+		System.out.println(address1);
 		
 		Address address2 = (Address)context.getBean("address2");
 		System.out.println(address2);
 
 		((ClassPathXmlApplicationContext)context).close();
 	}
-
 }
